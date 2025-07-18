ed0780c00855e3f9e97e89761b1b58f3952b3b7b
VRiek <riekkiv@gmail.com>
2025-07-18 14:05:40 +0300

    Enemy update

--- Assets/Scripts/MiniGame/EnemyMovement.cs
+++ Assets/Scripts/MiniGame/EnemyMovement.cs
@@ -5,6 +5,15 @@ public class EnemyMovement : MonoBehaviour
     public Transform player;
     private NavMeshAgent navMeshAgent;
 
+    public enum StateMachine
+    {
+        Idle,
+        Engage
+    }
+
+    public StateMachine currentState;
+    public float speed = 3f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,9 +23,40 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        switch (currentState)
+        {
+            case StateMachine.Idle:
+                Idle();
+                break;
+            case StateMachine.Engage:
+                Engage();
+                break;
+        }
+
+        bool playerSeen = Vector3.Distance(transform.position, player.position) < 10f;
+
+        if (!playerSeen && currentState != StateMachine.Idle)
+        {
+            currentState = StateMachine.Idle;
+        }
+        else if (playerSeen && currentState != StateMachine.Engage)
+        {
+            currentState = StateMachine.Engage;
+        }
+        /*if (player != null)
         {
             navMeshAgent.SetDestination(player.position);
-        }
+        }*/
     }
+
+    void Idle()
+    {
+        return;
+    }
+
+    void Engage()
+    {
+        navMeshAgent.SetDestination(player.position);
+    }
+
 }
